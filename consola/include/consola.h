085814b66c0403e9c7718a54b3c2da00348c54b1
Ian Feldman <ian@feldman.ar>
2023-04-13 21:46:29 -0300

    cargo mod. de consola

--- consola/include/consola.h
+++ consola/include/consola.h
@@ -10,6 +10,40 @@
 #include "serialize_utils_consola.h"
 #include <readline/readline.h>
 
+typedef struct {
+    uint32_t instruccion_long;
+    char* instruccion;
+    uint32_t size_param_1;
+    char* param_1;
+    uint32_t size_param_2;
+    char* param_2;
+    uint32_t size_param_3;
+    char* param_3;
+} t_instruccion_package_3;
+typedef struct {
+    uint32_t instruccion_long;
+    char* instruccion;
+    uint32_t size_param_1;
+    char* param_1;
+    uint32_t size_param_2;
+    char* param_2;
+} t_instruccion_package2;
+typedef struct {
+    uint32_t instruccion_long;
+    char* instruccion;
+    uint32_t size_param_1;
+    char* param_1;
+} t_instruccion_package_1;
+typedef struct {
+    uint32_t instruccion_long;
+    char* instruccion;
+} t_instruccion_package_0;
+
+
+void list_instruccions(t_list * instruccions);
+void process_instruccions(t_list * instruccions);
+int count_spaces(char* instruction_text);
+
 #endif
 
 
--- consola/src/consola.c
+++ consola/src/consola.c
@@ -31,6 +31,10 @@ int main(int argc, char ** argv){
 
 	log_info(logger, "Enviando paquete...");
 
+    
+    list_instruccions(instruccions);
+    process_instruccions(instruccions);
+    
 	socket_send_package(paquete, conexion_kernel);
     
     socket_end(conexion_kernel);
@@ -53,4 +57,70 @@ void load_basic_config( char* ip,
 
     
     
+}
+
+void list_instruccions(t_list *instruccions)
+{
+    char *directorio = getcwd(NULL, 0);
+    char *file_directory = malloc(strlen(directorio) + strlen("/cfg/instrucciones") + 1);
+    sprintf(file_directory, "%s/cfg/instrucciones", directorio);
+    
+    FILE *file_instruccions;
+    file_instruccions = fopen(file_directory, "r");
+    if (file_instruccions == NULL) {
+        printf("Error al abrir el archivo\n");
+        exit(1);
+    }
+   
+    char buffer[256];
+    while(fgets(buffer,sizeof(buffer),file_instruccions)){
+        char *line = strdup(buffer); // crear una copia de la línea
+        printf("lei: %s",line);
+        list_add(instruccions,line); //agrega los elementos leidos a la lista de instrucciones
+    }
+    
+
+  
+    fclose(file_instruccions);
+    free(file_directory);
+}
+
+void process_instruccions(t_list* instruccions){
+     for (int i = 0; i < list_size(instruccions); i++) {
+        char* instruccion = list_get(instruccions, i);
+        printf("%i _ %s\n", count_spaces(instruccion),list_get(instruccions, i)); 
+        int spaces = count_spaces(instruccion);//cuenta los espacios y dependiendo los espacios sera la cantidad de parametros 
+        switch (spaces) {
+            case 0:
+            
+                t_instruccion_package_0 inst;
+                inst.instruccion = malloc(strlen(instruccion)+1);
+                inst.instruccion_long = (strlen(instruccion)+1);
+                strcpy(inst.instruccion, instruccion);
+                break;
+            case 1:
+                printf("Ha seleccionado la opcion 1.\n");
+                break;
+            case 2:
+                printf("Ha seleccionado la opcion 2.\n");
+                break;
+            case 3:
+                printf("Ha seleccionado la opcion 3.\n");
+                break;
+            default:
+                printf("Error con la cantidad de parametros");
+                break;
+        }
+    }
+
+}
+
+int count_spaces(char *instruction_text) {
+    int count = 0;
+    for(int i = 0; i < strlen(instruction_text); i++) {
+        if(instruction_text[i] == ' ') {
+            count++;
+        }
+    }
+    return count;
 }
